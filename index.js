61ead9bc9e50651cda6b7492e0e1c07fde64657d
Linh <linhpham0519@gmail.com>
2025-05-06 17:40:29 -0400

    fixed pagination: not able to go back or forward after switching to page 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { renderPagination, fetchItems, loadItems, renderCards, hideLoading, show
 let currentPage = 1;
 const limit = 30; // Number of artworks per page
 let allArtworks = []; // Array to store all fetched artworks
-
+let currentArtworks = []; // Array to store the current page of artworks
 const fetchAllArtworks = async () => {
     showLoading(); // Show spinner while loading
 
@@ -15,28 +15,43 @@ const fetchAllArtworks = async () => {
     hideLoading(); // Hide spinner after loading
     console.log("after fetchItems()", artworks.length)
     allArtworks = artworks.filter(({image_id}) => image_id); // Filter out artworks without images);
+    currentArtworks = allArtworks;
     console.log(`Fetched ${allArtworks.length} artworks in total.`);
 };
 
 // Load artworks and render them
-const loadArtworks = (page) => {
+// const loadArtworks = (page) => {
+//     loadItems(
+//         allArtworks,
+//         page,
+//         limit,
+//         (artworks) => renderCards(artworks, 'artworks'), // Use renderCards for artworks
+//         '#content',
+//         // to handle page change triggered by pagination
+//         (newPage) => {
+//             currentPage = newPage;
+//             loadArtworks(currentPage);
+//         }
+//     );
+// };
+// Render the current page of artworks
+const renderCurrentPage = (page) => {
     loadItems(
-        allArtworks,
+        currentArtworks, // Use currentArtworks for pagination
         page,
         limit,
-        (artworks) => renderCards(artworks, 'artworks'), // Use renderCards for artworks
+        (artworks) => renderCards(artworks, 'artworks'),
         '#content',
-        // to handle page change triggered by pagination
         (newPage) => {
-            currentPage = newPage;
-            loadArtworks(currentPage);
+            currentPage = newPage; // Update the current page
+            renderCurrentPage(currentPage); // Reload the new page
         }
     );
 };
 
 // Fetch artworks and load the first page on page load
 fetchAllArtworks().then(() => {
-    loadArtworks(currentPage);
+    renderCurrentPage(currentPage);
 });
 
 //SEARCH FUNCTIONALITY
@@ -57,10 +72,12 @@ document.querySelector('#clearButton').addEventListener('click', () => {
     document.querySelector('#search').value = '';
     document.querySelector('#content').innerHTML = renderCards(allArtworks,"artworks");
     console.log("totalPages",allArtworks.length / limit);
-    renderPagination({
-        currentPage: 1,
-        totalPages: Math.ceil(allArtworks.length / limit),
-    });
+    currentArtworks = allArtworks;
+    renderCurrentPage(1);
+    // renderPagination({
+    //     currentPage: 1,
+    //     totalPages: Math.ceil(allArtworks.length / limit),
+    // });
 });
 
 
@@ -71,17 +88,18 @@ document.querySelector('#search').addEventListener('input', async () => {
     if (allArtworks.length === 0) {
         await fetchAllArtworks();
     }
-    const filteredArtworks = searchArtworks(searchTerm);
-
+    currentArtworks = searchArtworks(searchTerm);
+    console.log("filteredArtworks", currentArtworks);
+    renderCurrentPage(1);
     // Render the searched artworks
-    loadItems(
-        filteredArtworks,
-        1,
-        limit,
-        (artworks) => renderCards(artworks, 'artworks'),
-        '#content',
-        (newPage) => {
-            loadItems(filteredArtworks, newPage, limit, (artworks) => renderCards(artworks, 'artworks'), '#content', (newPage) => { });
-        }
-    );
+    // loadItems(
+    //     filteredArtworks,
+    //     1,
+    //     limit,
+    //     (artworks) => renderCards(artworks, 'artworks'),
+    //     '#content',
+    //     (newPage) => {
+    //         loadItems(filteredArtworks, newPage, limit, (artworks) => renderCards(artworks, 'artworks'), '#content', (newPage) => { });
+    //     }
+    // );
 });
